34a4f9b1a3110551d00c0abb5c30af0488786057
Paula Passet <p.s.passet@student.tue.nl>
2023-04-03 14:08:39 +0200

    organised main and clustering features

--- Denoise_Functions.py
+++ Denoise_Functions.py
@@ -12,18 +12,17 @@ def smoothen_fft(data,samplingRate):
         
         #Get the fft from the data
         yf_v1 = fft(y)
-        yf = 2.0/N * np.abs(yf_v1[0:N//2])
-        xf = fftfreq(N, fs)[:N//2]
 
         #Perform smoothing
         n = 5  # the larger n is, the smoother curve will be
         b = [1.0 / n] * n
         a = 1
+
         # yf_filter = lfilter(b, a, yf)
-        yf_filter = filtfilt(b, a, yf)
+        yf_filter = filtfilt(b, a, yf_v1)
         
         data_filtered.append(ifft(yf_filter))
 
-    return data_filtered
+    return data
 
 
