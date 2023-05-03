55120bb287f2652a862adc86d739cea57681ac6b
HugoGovaers <h.govaers@student.tue.nl>
2023-05-03 14:19:51 +0100

    -

--- Omron_visit_tests/Accelerometer/Vibration.py
+++ Omron_visit_tests/Accelerometer/Vibration.py
@@ -1,12 +1,12 @@
 import os
 
 
-fs = 400
-seconds = 5
+fs = 3200
+seconds = 10
 test_day = 'Accelerometer_Omron'
 file_type = '.csv'
 
-test_type = '4'
+test_type = '9'
 
 test_name = test_day + '_test' + str(test_type) + '_fs' + str(fs) + '_secs' + str(seconds) + file_type
 
