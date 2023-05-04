6c960c84df5d3bb210fbd301b24df9d0dc1e7a40
HugoGoofy <h.govaers@student.tue.nl>
2023-05-04 08:28:10 +0200

    final commit test day

--- Omron_visit_tests/Microphone/Microphone.py
+++ Omron_visit_tests/Microphone/Microphone.py
@@ -2,14 +2,12 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 import os.path
 
-save_path = "C:\Users\hugog\OneDrive - TU Eindhoven\TUE\Project\software\5ARIP10\Omron_visit_tests\Test_results\Microphone"
-print("test")
 fs = 44100  # Sample rate
-seconds = 5  # Duration of recording
-test_day = 'Microphone_Omron_Test_'
+seconds = 10  # Duration of recording
+test_day = 'Microphone_Omron_'
 file_type = '.wav'
 
-test_type = '1' #Name of test
+test_type = '17' #Name of test
 
 test_name = test_day + 'test_' + str(test_type) + '_fs' + str(fs) + '_secs' + str(seconds) + file_type
 
