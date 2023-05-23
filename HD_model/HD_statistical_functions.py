69964619ff021609ef230f7d157344dff4e5a562
FlyingFish760 <84115047+FlyingFish760@users.noreply.github.com>
2023-05-23 19:02:57 +0200

    add extract_fft_feature function

--- HD_model/HD_statistical_functions.py
+++ HD_model/HD_statistical_functions.py
@@ -345,6 +345,41 @@ def extract_time_features(data, x):
     feature_map = np.array(feature_map).T
     return feature_map
 
+def extract_fft_features(data, x):
+    """ 
+    extract statistical features of fft reshaped_data for every x sample points
+
+    return:
+    fft feature map (type: np array; 
+    size: (n_files * n_time_window(fs*secs/x)) * (5 features: means, maxs, mins, medians, stds))
+    
+    """
+    # Reshape the reshaped_data to split them every x sample points
+    reshaped_data = np.reshape(data, (data.shape[0], -1, x))
+    
+    #calculate fft of reshaped_data
+    reshaped_data_fft = np.abs(np.fft.fft(reshaped_data, axis=-1))
+
+    # calculate statistical features of fft reshaped_data
+    # shape:[11*(2646000/x)*x]-> shape:[11*(2646000/x)]
+    fft_means = np.mean(reshaped_data_fft, axis=-1)
+    fft_maxs = np.max(reshaped_data_fft, axis=-1)
+    fft_mins = np.min(reshaped_data_fft, axis=-1)
+    fft_medians = np.median(reshaped_data_fft, axis=-1)
+    fft_stds = np.std(reshaped_data_fft, axis=-1)
+
+    # integrade reshaped_data to a high dimensional feature matrix: size: (n_speeds * n_time_window) * (10(5 original 
+    # features + 5 fft features)); 
+    # sequence of features: mic_means,mic_maxs...fft_means,fft_maxs...
+    fft_feats = []
+    for i in [fft_means, fft_maxs, fft_mins, fft_medians, fft_stds]:
+        i = i.flatten()
+        fft_feats.append(i)
+
+    fft_feats = np.array(fft_feats).T
+
+    return fft_feats
+
 def plot_basics(anom_means, anom_stds, anom_maxs, anom_mins, anom_medians, norm_means, norm_stds, norm_maxs, norm_mins, norm_medians):
     
     fig, ax = plt.subplots(5, 2,figsize=(7,15), tight_layout=True)
--- HD_model/split_extract_vibr_features.py
+++ HD_model/split_extract_vibr_features.py
@@ -1,37 +1,34 @@
-import numpy as np
-
-def extract_fft_features(data, x):
+def extract_fft_features(reshaped_data, x):
     """ 
-    extract statistical features of fft data for every x sample points
+    extract statistical features of fft reshaped_data for every x sample points
 
     return:
     fft feature map (type: np array; 
     size: (n_files * n_time_window(fs*secs/x)) * (5 features: means, maxs, mins, medians, stds))
     
     """
-    #calculate fft of data
-    reshaped_data_fft = np.abs(np.fft.fft(reshaped_data, axis=2))
+    # Reshape the reshaped_data to split them every x sample points
+    reshaped_reshaped_data = np.reshape(reshaped_data, (reshaped_data.shape[0], -1, x))
+    
+    #calculate fft of reshaped_data
+    reshaped_data_fft = np.abs(np.fft.fft(reshaped_data, axis=-1))
 
-    # calculate statistical features of fft data
+    # calculate statistical features of fft reshaped_data
     # shape:[11*(2646000/x)*x]-> shape:[11*(2646000/x)]
-    mic_fft_means = np.mean(reshaped_data_fft, axis=-1)
-    mic_fft_maxs = np.max(reshaped_data_fft, axis=-1)
-    mic_fft_mins = np.min(reshaped_data_fft, axis=-1)
-    mic_fft_medians = np.median(reshaped_data_fft, axis=-1)
-    mic_fft_stds = np.std(reshaped_data_fft, axis=-1)
+    fft_means = np.mean(reshaped_data_fft, axis=-1)
+    fft_maxs = np.max(reshaped_data_fft, axis=-1)
+    fft_mins = np.min(reshaped_data_fft, axis=-1)
+    fft_medians = np.median(reshaped_data_fft, axis=-1)
+    fft_stds = np.std(reshaped_data_fft, axis=-1)
 
-    # integrade data to a high dimensional feature matrix: size: (n_speeds * n_time_window) * (10(5 original 
+    # integrade reshaped_data to a high dimensional feature matrix: size: (n_speeds * n_time_window) * (10(5 original 
     # features + 5 fft features)); 
-    # sequence of features: mic_means,mic_maxs...mic_fft_means,mic_fft_maxs...
-    mic_feats = []
-    for i in [mic_means, mic_maxs, mic_mins, mic_medians, mic_stds, 
-              mic_fft_means, mic_fft_maxs, mic_fft_mins, mic_fft_medians, mic_fft_stds]:
+    # sequence of features: mic_means,mic_maxs...fft_means,fft_maxs...
+    fft_feats = []
+    for i in [fft_means, fft_maxs, fft_mins, fft_medians, fft_stds]:
         i = i.flatten()
-        mic_feats.append(i)
+        fft_feats.append(i)
 
-    mic_feats = np.array(mic_feats).T
+    fft_feats = np.array(fft_feats).T
 
-    return
-
-
-
+    return fft_feats
